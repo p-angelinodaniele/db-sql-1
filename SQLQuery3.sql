a4b92be1ba429509efa42caf9f694b361f330808
P.angelino <p.angelinodaniele@gmail.com>
2022-04-29 16:12:13 +0200

    Finito con bonus

---
+++ SQLQuery3.sql
@@ -0,0 +1,51 @@
+﻿SELECT * 
+FROM teachers;
+
+
+SELECT head_of_department 
+from departments;
+
+SELECT *
+FROM degrees 
+WHERE level = 'magistrale';
+
+SELECT *
+FROM students
+WHERE name = 'Marco';
+
+select * 
+from courses
+where cfu >= 12;
+
+select * 
+from courses
+where cfu >= 10 OR cfu <= 5;
+
+select *
+from courses
+where period = 'I semestre' AND year=1;
+
+
+
+SELECT *
+FROM courses
+where website IS NULL;
+
+SELECT *
+FROM teachers
+where phone IS NOT NULL;
+
+
+
+SELECT *
+FROM students
+where name like 'E%';
+
+
+select count (*)
+FROM teachers
+where name like 'E%';
+
+SELECT count (*)
+FROM teachers
+where phone is null;
